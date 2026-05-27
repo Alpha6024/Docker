dc0ba7b79219fad5fdff536ecbf55bf5d3f93ca7
Alpha6024 <pranavborkar40@gmail.com>
2026-05-27 11:53:41 +0530

    action testing 01

--- design/src/App.jsx
+++ design/src/App.jsx
@@ -43,7 +43,7 @@ export default function App() {
           onClick={handleSend}
           className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
         >
-          Send
+          Send Note
         </button>
       </div>
 
